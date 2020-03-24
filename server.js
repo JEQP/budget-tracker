df0b8cbc8c04f1cc1ac30a270bc90cf1688866c1
James Pearce <jamesquintanapearce@gmail.com>
2020-03-24 11:55:13 +1100

    starting with class files

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-
-require('dotenv').config()
 const express = require("express");
-const path = require("path");
 const logger = require("morgan");
-const mongojs = require("mongojs");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3500;
+const PORT = 3000;
 
 const app = express();
-const Budget = require("./models/budgetModel.js");
 
 app.use(logger("dev"));
 
@@ -20,23 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-const databaseUrl = process.env.MONGODB_URI;
-const collections = ["budgets"];
-
-const db = mongojs(databaseUrl, collections);
-
-db.on("error", error => {
-    console.log("Database Error:", error);
-  });
-
-// routes here
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
